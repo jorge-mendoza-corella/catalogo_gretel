738b698e76b176bd65facee99d4408eea2dba5c8
Jorge Mendoza-Ubuntu <mencor.corp@gmail.com>
2025-01-21 13:43:45 -0600

    elimine lo que no sirve

--- composables/use-contracts.ts
+++
@@ -1,5 +0,0 @@
-// define app states
-import type { Contract } from '~~/types/use-contracts'
-
-// export notifications state
-export const useContracts = () => useState<Contract[]>('contract', () => ([]))
--- composables/use-tags.ts
+++
@@ -1,5 +0,0 @@
-// define app states
-import type { Tag } from '~~/types/use-tag'
-
-// export notifications state
-export const useTags = () => useState<Tag[]>('tag', () => ([]))
