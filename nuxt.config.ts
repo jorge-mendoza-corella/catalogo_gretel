84771edd32cee0e383778adf4cd283d97e813e66
Jorge Mendoza-Ubuntu <mencor.corp@gmail.com>
2025-01-22 09:18:59 -0600

    puse el dist

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/',
+  },
+  generate: {
+    dir: 'dist',
+  },
   nitro: {
     preset: 'vercel', // Cambia esto si tienes algo personalizado
   },
