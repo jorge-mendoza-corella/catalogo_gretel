3ea36ae6b0cd6ff4937f1814c2554967137f8448
Jorge Mendoza-Ubuntu <mencor.corp@gmail.com>
2025-01-21 20:37:51 -0600

    configurando el runtime a node 18

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,10 @@
 export default defineNuxtConfig({
-  target: 'static',
-  ssr: false,
-
   nitro: {
-    preset: 'vercel',
-    node: '18' // Asegura que el runtime sea compatible
+    preset: 'vercel', // Configura Nitro para Vercel
+    vercel: {
+      runtime: 'nodejs18.x', // Fuerza el uso de Node.js 18.x
+    },
   },
-
-  app: {
-    baseURL: '/',
-    head: { /* tu configuración de meta y head */ },
-  },
-
-  css: [
-    'primevue/resources/themes/lara-light-teal/theme.css',
-    'primevue/resources/primevue.css',
-    'primeicons/primeicons.css',
-    'primeflex/primeflex.css',
-  ],
 
   compatibilityDate: '2025-01-21',
 });
