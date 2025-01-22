0f8a21a336af6a55f88ad1136078591cd850dafe
Jorge Mendoza-Ubuntu <mencor.corp@gmail.com>
2025-01-21 19:32:30 -0600

    elimine cosas que no son neesarias

---
+++ .vercel/output/functions/__nitro.func/chunks/build/client.manifest.mjs
@@ -0,0 +1,195 @@
+const client_manifest = {
+  "_BaglTkzI.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BaglTkzI.js",
+    "name": "vue.-sixQ7xP",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "COK_B08H.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BaglTkzI.js"
+    ],
+    "css": [
+      "error-404.C3V-3Mc4.css"
+    ]
+  },
+  "error-404.C3V-3Mc4.css": {
+    "file": "error-404.C3V-3Mc4.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CH5Gpqgc.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_BaglTkzI.js"
+    ],
+    "css": [
+      "error-500.dGVH929u.css"
+    ]
+  },
+  "error-500.dGVH929u.css": {
+    "file": "error-500.dGVH929u.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BLllPW4K.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.CAzHFZpx.css"
+    ],
+    "assets": [
+      "Inter-roman.var.C-r5W2Hj.woff2",
+      "Inter-italic.var.DhD-tpjY.woff2",
+      "primeicons.DMOk5skT.eot",
+      "primeicons.C6QP2o4f.woff2",
+      "primeicons.WjwUDZjB.woff",
+      "primeicons.MpK4pl85.ttf",
+      "primeicons.Dr5RGzOO.svg"
+    ]
+  },
+  "entry.CAzHFZpx.css": {
+    "file": "entry.CAzHFZpx.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "Inter-roman.var.C-r5W2Hj.woff2": {
+    "file": "Inter-roman.var.C-r5W2Hj.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "Inter-italic.var.DhD-tpjY.woff2": {
+    "file": "Inter-italic.var.DhD-tpjY.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "primeicons.DMOk5skT.eot": {
+    "file": "primeicons.DMOk5skT.eot",
+    "resourceType": "font",
+    "mimeType": "font/eot"
+  },
+  "primeicons.C6QP2o4f.woff2": {
+    "file": "primeicons.C6QP2o4f.woff2",
+    "resourceType": "font",
+    "mimeType": "font/woff2"
+  },
+  "primeicons.WjwUDZjB.woff": {
+    "file": "primeicons.WjwUDZjB.woff",
+    "resourceType": "font",
+    "mimeType": "font/woff"
+  },
+  "primeicons.MpK4pl85.ttf": {
+    "file": "primeicons.MpK4pl85.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "primeicons.Dr5RGzOO.svg": {
+    "file": "primeicons.Dr5RGzOO.svg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml"
+  },
+  "node_modules/primeicons/fonts/primeicons.eot": {
+    "resourceType": "font",
+    "mimeType": "font/eot",
+    "file": "primeicons.DMOk5skT.eot",
+    "src": "node_modules/primeicons/fonts/primeicons.eot"
+  },
+  "node_modules/primeicons/fonts/primeicons.svg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/svg+xml",
+    "file": "primeicons.Dr5RGzOO.svg",
+    "src": "node_modules/primeicons/fonts/primeicons.svg"
+  },
+  "node_modules/primeicons/fonts/primeicons.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "primeicons.MpK4pl85.ttf",
+    "src": "node_modules/primeicons/fonts/primeicons.ttf"
+  },
+  "node_modules/primeicons/fonts/primeicons.woff": {
+    "resourceType": "font",
+    "mimeType": "font/woff",
+    "file": "primeicons.WjwUDZjB.woff",
+    "src": "node_modules/primeicons/fonts/primeicons.woff"
+  },
+  "node_modules/primeicons/fonts/primeicons.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "primeicons.C6QP2o4f.woff2",
+    "src": "node_modules/primeicons/fonts/primeicons.woff2"
+  },
+  "node_modules/primevue/resources/themes/lara-light-teal/fonts/Inter-italic.var.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Inter-italic.var.DhD-tpjY.woff2",
+    "src": "node_modules/primevue/resources/themes/lara-light-teal/fonts/Inter-italic.var.woff2"
+  },
+  "node_modules/primevue/resources/themes/lara-light-teal/fonts/Inter-roman.var.woff2": {
+    "resourceType": "font",
+    "mimeType": "font/woff2",
+    "file": "Inter-roman.var.C-r5W2Hj.woff2",
+    "src": "node_modules/primevue/resources/themes/lara-light-teal/fonts/Inter-roman.var.woff2"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BvZbUmv3.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "index.DLmlvLq5.css"
+    ]
+  },
+  "index.DLmlvLq5.css": {
+    "file": "index.DLmlvLq5.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
---
+++ .vercel/output/functions/__nitro.func/index.mjs
@@ -0,0 +1,6 @@
+import process from 'node:process';globalThis._importMeta_={url:import.meta.url,env:process.env};export { l as default } from './chunks/nitro/nitro.mjs';
+import 'node:http';
+import 'node:https';
+import 'node:fs';
+import 'node:path';
+//# sourceMappingURL=index.mjs.map
